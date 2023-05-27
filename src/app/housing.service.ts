6090f0259bab098977e9b7fc8cfde929adbc52f6
Clayton Priestley <clayton.priestley@gmail.com>
2023-05-27 12:31:27 -0500

    Add Apply Form - add form to details component template - add handler to invoke new method submitApplication() function on submit

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -117,4 +117,8 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: String, lastName: String, email: String) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName : ${lastName}, email: ${email}.`);
+  }
 }
